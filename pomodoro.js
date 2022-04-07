e1423328a6562a46809111913041d778f041deec
ryusiin <risnworlds@gmail.com>
2022-04-07 21:23:07 +0900

    2022-04-07

--- pomodoro.js
+++ pomodoro.js
@@ -7,6 +7,7 @@
 
   this.setInterval(this.Update, 100);
   this.ShowCurrentTime();
+  this.ShowEndTime();
 }
 function Update() {
   this.ShowCurrentTime();
@@ -18,6 +19,7 @@ function Update() {
 // :: 수치 및 UI 초기화
 // :: UI
 let TEXT_CurrentTime; // : UI 현재 시간
+let TEXT_EndTime; // : UI 종료 시간
 let TEXT_Timer; // : UI 타이머
 let TEXT_State; // : UI 상태
 let SECTION_Progress; // : UI 진행도
@@ -25,6 +27,11 @@ let SECTION_MainTimer; // : UI 메인 타이머
 let SECTION_Settings; // : UI 설정
 let SECTION_Modal; // : UI 모달
 let BUTTON_Auto; // : UI 버튼 Auto
+let INPUT_Time_Focus; // : UI Input Focus
+let INPUT_Time_ShortBreak; // : UI Input Short Break
+let INPUT_Time_LongBreak; // : UI Input Long Break
+let INPUT_Time_Finish_Hour; // : UI Input Finish Hour
+let INPUT_Time_Finish_Minute; // : UI Input Finish Minute
 // :: Audio
 let nSound;
 let SOUND_AlarmEndBreakLong;
@@ -47,13 +54,23 @@ let iCurrentTime_Second; // : 현재 초
 function InitStatus() {
   // :: UI
   this.TEXT_CurrentTime = document.getElementById("TEXT_CurrentTime"); // : 현재 시간
+  this.TEXT_EndTime = document.getElementById("TEXT_EndTime"); // : 종료 시간
   this.TEXT_Timer = document.getElementById("TEXT_Timer"); // : 현재 타이머
   this.TEXT_State = document.getElementById("TEXT_State"); // : 현재 상태
   this.SECTION_Progress = document.getElementById("SECTION_Progress"); // : 현재 진행도
   this.SECTION_MainTimer = document.getElementById("SECTION_MainTimer"); // : 메인 타이머
   this.SECTION_Settings = document.getElementById("SECTION_Settings"); // : 설정
-  this.BUTTON_Auto = document.getElementById("BUTTON_Auto"); // : 버튼 Auto
   this.SECTION_Modal = document.getElementById("SECTION_Modal"); // : 모달
+  this.BUTTON_Auto = document.getElementById("BUTTON_Auto"); // : 버튼 Auto
+  this.INPUT_Time_Focus = document.getElementById("INPUT_Time_Focus"); // : Input Time Focus
+  this.INPUT_Time_ShortBreak = document.getElementById("INPUT_Time_ShortBreak"); // : Input Time Short Break
+  this.INPUT_Time_LongBreak = document.getElementById("INPUT_Time_LongBreak"); // : Input Time Long Break
+  this.INPUT_Time_Finish_Hour = document.getElementById(
+    "INPUT_Time_Finish_Hour",
+  ); // : Input Time Finish Hour
+  this.INPUT_Time_Finish_Minute = document.getElementById(
+    "INPUT_Time_Finish_Minute",
+  ); // : Input Time Finish Mintue
 
   // :: 사운드
   this.SOUND_AlarmEndFocus = new Audio("./Sound/sound_alarm_end_focus.mp3");
@@ -76,12 +93,15 @@ function InitStatus() {
 
   // :: 현재 상태
   this.nState = new Array("Focus", "Short Break", "Long Break");
+
   // :: 현재 타이머
-  this.iTimeFocus = 1;
-  this.iTimeBreakShort = 1;
-  this.iTimeBreakLong = 1;
+  this.iTimeFocus = 25;
+  this.iTimeBreakShort = 5;
+  this.iTimeBreakLong = 30;
   this.iDoTimer = false;
   this.DoAuto();
+
+  this.LoadTime();
 
   // :: 현재 진행도
   this.iProgress = 0;
@@ -90,13 +110,107 @@ function InitStatus() {
   this.SetTimerFocus();
 }
 
+function UpdateSetting_CategoryTime() {
+  this.INPUT_Time_Focus.value = this.iTimeFocus;
+  this.INPUT_Time_ShortBreak.value = this.iTimeBreakShort;
+  this.INPUT_Time_LongBreak.value = this.iTimeBreakLong;
+}
+function UpdateSetting_FinishTime() {
+  this.INPUT_Time_Finish_Hour.value = this.iEndTime_Hour;
+  this.INPUT_Time_Finish_Minute.value = this.iEndTime_Minute;
+}
+function InputSetting_CategoryTime() {
+  this.ChangeTimeFocus(this.INPUT_Time_Focus.value);
+  this.ChangeTimeShortBreak(this.INPUT_Time_ShortBreak.value);
+  this.ChangeTimeLongBreak(this.INPUT_Time_LongBreak.value);
+}
+function InputSetting_FinishTime() {
+  this.ChangeTimeFinish(
+    this.INPUT_Time_Finish_Hour.value,
+    this.INPUT_Time_Finish_Minute.value,
+  );
+}
+function ChangeTimeFinish(_hour, _minute) {
+  this.iEndTime_Hour = Number(_hour);
+  this.iEndTime_Minute = Number(_minute);
+  this.SaveFinishTime();
+}
+function ChangeTimeFocus(_value) {
+  this.iTimeFocus = Number(_value);
+  this.SaveTime();
+}
+function ChangeTimeShortBreak(_value) {
+  this.iTimeBreakShort = Number(_value);
+  this.SaveTime();
+}
+function ChangeTimeLongBreak(_value) {
+  this.iTimeBreakLong = Number(_value);
+  this.SaveTime();
+}
+function SaveFinishTime() {
+  window.localStorage.setItem("time_finish_hour", this.iEndTime_Hour);
+  window.localStorage.setItem("time_finish_minute", this.iEndTime_Minute);
+}
+function SaveTime() {
+  window.localStorage.setItem("time_focus", this.iTimeFocus);
+  window.localStorage.setItem("time_short_break", this.iTimeBreakShort);
+  window.localStorage.setItem("time_long_break", this.iTimeBreakLong);
+}
+function LoadTime() {
+  if (window.localStorage.getItem("time_focus") === null) {
+    window.localStorage.setItem("time_focus", this.iTimeFocus);
+  } else {
+    this.iTimeFocus = window.localStorage.getItem("time_focus");
+  }
+
+  if (window.localStorage.getItem("time_short_break") === null) {
+    window.localStorage.setItem("time_short_break", this.iTimeBreakShort);
+  } else {
+    this.iTimeBreakShort = window.localStorage.getItem("time_short_break");
+  }
+
+  if (window.localStorage.getItem("time_long_break") === null) {
+    window.localStorage.setItem("time_long_break", this.iTimeBreakLong);
+  } else {
+    this.iTimeBreakLong = window.localStorage.getItem("time_long_break");
+  }
+}
+
 function AddButtonScenarios() {
   this.AddButtonSecnario_Modal();
+
+  this.AddButtonScenario_Input_Focus();
+  this.AddButtonScenario_Input_ShortBreak();
+  this.AddButtonScenario_Input_LongBreak();
+
+  this.AddButtonScenario_Input_FinishTime();
 }
 function AddButtonSecnario_Modal() {
   this.SECTION_Modal.addEventListener("click", (e) => {
-    if (e.target === this.SECTION_Modal)
-      this.SECTION_Modal.style.display = "none";
+    if (e.target === this.SECTION_Modal) this.HideSettings();
+  });
+}
+function AddButtonScenario_Input_Focus() {
+  this.INPUT_Time_Focus.addEventListener("change", (e) => {
+    this.InputSetting_CategoryTime();
+  });
+}
+function AddButtonScenario_Input_ShortBreak() {
+  this.INPUT_Time_ShortBreak.addEventListener("change", (e) => {
+    this.InputSetting_CategoryTime();
+  });
+}
+function AddButtonScenario_Input_LongBreak() {
+  this.INPUT_Time_LongBreak.addEventListener("change", (e) => {
+    this.InputSetting_CategoryTime();
+  });
+}
+function AddButtonScenario_Input_FinishTime() {
+  this.INPUT_Time_Finish_Hour.addEventListener("change", (e) => {
+    this.InputSetting_FinishTime();
+  });
+  this.INPUT_Time_Finish_Minute.addEventListener("change", (e) => {
+    this.InputSetting_FinishTime();
   });
 }
 
@@ -111,16 +225,54 @@ function ShowCurrentTime() {
   const minutes = ("0" + now.getMinutes()).slice(-2);
   const seconds = ("0" + now.getSeconds()).slice(-2);
   const currentTime = `${year}-${month}-${date}(${day}) ${hours}:${minutes}:${seconds}`;
+
   this.TEXT_CurrentTime.innerHTML = currentTime;
+
+  if (
+    now.getSeconds() == 0 &&
+    now.getHours() == this.iEndTime_Hour &&
+    now.getMinutes() == this.iEndTime_Minute
+  ) {
+    this.SOUND_AlarmEndBreakLong.play();
+  }
+}
+
+let iEndTime_Hour;
+let iEndTime_Minute;
+function ShowEndTime() {
+  const now = new Date();
+  if (window.localStorage.getItem("time_finish_hour") == null) {
+    this.iEndTime_Hour = now.getHours();
+  } else {
+    this.iEndTime_Hour = window.localStorage.getItem("time_finish_hour");
+  }
+
+  if (window.localStorage.getItem("time_finish_minute") == null) {
+    this.iEndTime_Minute = now.getMinutes();
+  } else {
+    this.iEndTime_Minute = window.localStorage.getItem("time_finish_minute");
+  }
+
+  const hours = ("0" + this.iEndTime_Hour).slice(-2);
+  const minutes = ("0" + this.iEndTime_Minute).slice(-2);
+
+  this.TEXT_EndTime.innerHTML = `Finish Time : ${hours}:${minutes}`;
 }
 
 function ShowSettings() {
   this.SECTION_Modal.style.display = "block";
   this.SECTION_Settings.style.display = "block";
+
+  this.UpdateSetting_CategoryTime();
+  this.UpdateSetting_FinishTime();
 }
+
 function HideSettings() {
   this.SECTION_Modal.style.display = "none";
   this.SECTION_Settings.style.display = "none";
+
+  this.ResetTimer();
+  this.ShowEndTime();
 }
 
 function ResetPomodoro() {
@@ -141,8 +293,8 @@ function DoTimer() {
     const now = new Date();
 
     // :: 골 더하기
-    now.setMinutes(now.getMinutes() + this.iCurrentTime_Minute);
-    now.setSeconds(now.getSeconds() + this.iCurrentTime_Second);
+    now.setMinutes(now.getMinutes() + Number(this.iCurrentTime_Minute));
+    now.setSeconds(now.getSeconds() + Number(this.iCurrentTime_Second));
 
     // ::  목표 저장
     this.iGoalTime = now;
@@ -152,9 +304,11 @@ function DoTimer() {
     this.SOUND_TimerOff.play();
   }
 }
+
 function DoAuto() {
   this.iDoAuto = this.BUTTON_Auto.checked;
 }
+
 function ReduceTimer() {
   this.UpdateTimer();
   if (this.iCurrentTime_Minute <= 0 && this.iCurrentTime_Second <= 0) {
@@ -168,11 +322,13 @@ function ReduceTimer() {
     this.iCurrentTime_Second = seconds;
   }
 }
+
 function UpdateTimer() {
   const minutes = ("0" + this.iCurrentTime_Minute).slice(-2);
   const seconds = ("0" + this.iCurrentTime_Second).slice(-2);
   this.TEXT_Timer.innerHTML = `${minutes}:${seconds}`;
 }
+
 function SetTimerFocus() {
   this.iState = 0;
   this.iCurrentTime_Minute = this.iTimeFocus;
@@ -184,6 +340,7 @@ function SetTimerFocus() {
 
   this.PlusProgress();
 }
+
 function SetTimerBreakShort() {
   this.iState = 1;
   this.iCurrentTime_Minute = this.iTimeBreakShort;
@@ -193,6 +350,7 @@ function SetTimerBreakShort() {
 
   this.SECTION_MainTimer.className = "color_shortBreak";
 }
+
 function SetTimerBreakLong() {
   this.iState = 2;
   this.iCurrentTime_Minute = this.iTimeBreakLong;
@@ -204,6 +362,7 @@ function SetTimerBreakLong() {
 
   this.PlusProgress();
 }
+
 function EndTimer() {
   this.iDoTimer = false;
   // :: 사운드 재생
@@ -219,6 +378,7 @@ function EndTimer() {
   // :: Auto일 때 자동 실행
   if (this.iDoAuto) this.DoTimer();
 }
+
 function ResetProgress() {
   this.iProgress = 0;
 
@@ -232,10 +392,12 @@ function ResetProgress() {
       this.GetColor(-1);
   }
 }
+
 function PlusProgress() {
   this.iProgress += 1;
   this.UpdateProgress();
 }
+
 function UpdateProgress() {
   for (let index = 0; index < this.iProgress; index++) {
     this.SECTION_Progress.children[index].style.backgroundColor = this.GetColor(
@@ -249,6 +411,21 @@ function GetColor(_index) {
   if (this.iProgress === _index) return "#D44949";
   return "#DB7B7B";
 }
+
 function UpdateState() {
   this.TEXT_State.innerHTML = this.nState[this.iState];
+}
+
+function ResetTimer() {
+  // :: 이미 진행중이면 return
+  if (this.iDoTimer) return;
+
+  if (this.iState == 0) {
+    this.iCurrentTime_Minute = this.iTimeFocus;
+  } else if (this.iState == 1) {
+    this.iCurrentTime_Minute = this.iTimeBreakShort;
+  } else if (this.iState == 2) {
+    this.iCurrentTime_Minute = this.iTimeBreakLong;
+  }
+  this.UpdateTimer();
 }
